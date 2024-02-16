d06fe4b17fd661cffce06726e56b0c3a113e3863
Volodymyr Petrenko <mailbox.github@gmail.com>
2024-02-16 11:14:32 +0200

    ready for check

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ class Name(Field):
 
 class Phone(Field):
     def settt(self, value):
-        return int(value)
-
+        if len(value) == 10:
+            return (int(value))
+        else:
+            return None
 
 
 class Record:
@@ -23,31 +25,27 @@ class Record:
         self.phones = []
 
     def add_phone(self, value):
-        tt=Phone(value)   
-        self.phones.append(tt)
+        number=Phone(value) 
+        if number.settt(value):
+            self.phones.append(number)
 
-    def edit_phone(self,old,new):
-        for p in self.phones:
-            if p.value == old:
-                p.value = new
+    def edit_phone(self,old_num,new_num):
+        for item in self.phones:
+            if item.value == old_num:
+                item.value = new_num
 
     def find_phone(self, phone):
         for p in self.phones:
             if p.value == phone:
                 return p.value
 
-
-    # реалізація класу
-
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
     
-    def rer(self):
-        return {self.name.value : [p.value for p in self.phones]}
-    
+
 class AddressBook(UserDict):
-    def add_record(self, iii):
-        self.data[iii.name.value] = iii
+    def add_record(self, record):
+        self.data[record.name.value] = record
 
     
     def find(self, name):
@@ -59,11 +57,8 @@ class AddressBook(UserDict):
         pass
 
 
-
-
 # Створення нової адресної книги
 book = AddressBook()
-
 
 # Створення запису для John
 john_record = Record("John")
@@ -73,22 +68,18 @@ john_record.add_phone("5555555555")
 # # Додавання запису John до адресної книги
 book.add_record(john_record)
 
-
 # # Створення та додавання нового запису для Jane
 jane_record = Record("Jane")
 jane_record.add_phone("9876543210")
 book.add_record(jane_record)
 
-
 # # Виведення всіх записів у книзі
 for name, record in book.data.items():
     print(record)
 
-
 # # Знаходження та редагування телефону для John
 john = book.find("John")
 john.edit_phone("1234567890", "1112223333")
-
 
 print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
 
